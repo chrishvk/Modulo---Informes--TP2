b2eac839a5c252fdf95f063a81db0364ac2db13d
Kale <gian21022001@gmail.com>
2024-05-20 18:56:28 -0500

    Route add

--- Dentito-master/angular/src/app/dashboard-estudiante/pages/informes/informes.component.ts
+++ Dentito-master/angular/src/app/dashboard-estudiante/pages/informes/informes.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { TitleComponent } from '../../../shared/title/title.component';
+import { Router } from '@angular/router';
 
 @Component({
   standalone: true,
@@ -7,4 +8,10 @@ import { TitleComponent } from '../../../shared/title/title.component';
   templateUrl: './informes.component.html',
   styles: ``,
 })
-export default class InformesComponent {}
+export default class InformesComponent {
+  constructor(private router: Router) {}
+  onEyeClick() {
+    this.router.navigate(['/dashboard-estudiante/view']);
+    console.log('Redireccionado EyeClick');
+  }
+}
--- Dentito-master/angular/src/app/dashboard-estudiante/pages/informes/view/view.component.ts
+++ Dentito-master/angular/src/app/dashboard-estudiante/pages/informes/view/view.component.ts
@@ -17,26 +17,36 @@ export default class EditComponent {
         scale: 2,
         useCORS: true,
       }).then((canvas) => {
-        const imgWidth = 210;
-        const imgHeight = (canvas.height * imgWidth) / canvas.width;
+        const pageWidth = 210;
+        const pageHeight = 297;
+        let imgWidth = 210;
+        let imgHeight = (canvas.height * imgWidth) / canvas.width;
 
         const contentDataURL = canvas.toDataURL('image/png');
-        const pdf = new jsPDF({
-          orientation: 'p',
-          unit: 'mm',
-          format: 'a4',
-        });
+        const pdf = new jsPDF('p', 'mm', 'a4');
 
-        const margin = 0;
+        const margin = (pageWidth - imgWidth) / 2;
+        const verticalMargin = (pageHeight - imgHeight) / 2;
+
+        if (imgHeight > pageHeight) {
+          imgHeight = pageHeight;
+          imgWidth = (canvas.width * imgHeight) / canvas.height;
+        }
+
         pdf.addImage(
           contentDataURL,
           'PNG',
           margin,
-          margin,
-          imgWidth - 2 * margin,
+          verticalMargin > 0 ? verticalMargin : 0,
+          imgWidth,
           imgHeight
         );
-        pdf.save('informe.pdf');
+
+        const pdfBlob = pdf.output('blob');
+
+        const pdfUrl = URL.createObjectURL(pdfBlob);
+
+        window.open(pdfUrl, '_blank');
       });
     }
   }
